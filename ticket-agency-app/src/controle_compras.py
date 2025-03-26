04a92db8cad2d03d83e3032dc7495ba228e0f6fb
titoconte <titoconte@gmail.com>
2025-03-26 17:17:46 -0300

    Revisão da tela de compras

--- ticket-agency-app/src/controle_compras.py
+++ ticket-agency-app/src/controle_compras.py
@@ -1,45 +1,31 @@
 import streamlit as st
 from utils.google_sheets import *
+from utils import google_sheets as gs
 
 def controle_compras():
-    st.title("Controle de Compras")
+    st.subheader("Controle de Compras")
+    # ler aba de milheiros na coluna numero milheiro coletar todos os códigos e exibir em lista suspensa
+    milheiro_code = gs.read_sheet('Milheiros')['Número Milheiro'].values
+    numero_milheiro_compra = st.selectbox("Número Milheiro", milheiro_code, key="numero_milheiro_compra")
+    quantidade_milhas_compra = st.text_input("Quantidade de Milhas", key="quantidade_milhas_compra")
+    programa_compra = st.text_input("Programa", key="programa_compra")
+    preco_1k_milhas_compra = st.text_input("Preço por 1k Milhas", key="preco_1k_milhas_compra")
+    qtd_emissoes_compra = st.text_input("Quantidade de Emissões", key="qtd_emissoes_compra")
+    saldo_utilizado_compra = st.text_input("Saldo Utilizado", key="saldo_utilizado_compra")
+    saldo_remanescente_compra = st.text_input("Saldo Remanescente", key="saldo_remanescente_compra")
+    valor_total_compra = st.text_input("Valor Total", key="valor_total_compra")
+    # data_compra = st.text_input("Data da Compra", key="data_compra")
+    taxa_compra = st.text_input("Taxa", key="taxa_compra")
+    valor_total_pix_compra = st.text_input("Valor Total PIX", key="valor_total_pix_compra")
+    pix_enviado_compra = st.text_input("PIX Enviado", key="pix_enviado_compra")
 
-    # Aba 1 - Relatório de Compras
-    if st.sidebar.radio("Selecione uma aba", ["Relatório de Compras", "Inserir Informações", "Consultar Compra", "Alterar Informação"]) == "Relatório de Compras":
-        st.subheader("Relatório de Compras")
-        # Aqui você pode adicionar a lógica para exibir o relatório de compras
-        compras_data = read_data("compras")  # Função para ler dados da aba "compras"
-        st.write(compras_data)
-
-    # Aba 2 - Inserir Informações
-    elif st.sidebar.radio("Selecione uma aba", ["Relatório de Compras", "Inserir Informações", "Consultar Compra", "Alterar Informação"]) == "Inserir Informações":
-        st.subheader("Inserir Informações de Compras")
-        # Campos para inserir informações de compras
-        produto = st.text_input("Produto")
-        quantidade = st.number_input("Quantidade", min_value=1)
-        preco = st.number_input("Preço", min_value=0.0, format="%.2f")
-
-        if st.button("Inserir"):
-            insert_data("compras", {"produto": produto, "quantidade": quantidade, "preco": preco})  # Função para inserir dados
-            st.success("Informações inseridas com sucesso!")
-
-    # Aba 3 - Consultar Compra
-    elif st.sidebar.radio("Selecione uma aba", ["Relatório de Compras", "Inserir Informações", "Consultar Compra", "Alterar Informação"]) == "Consultar Compra":
-        st.subheader("Consultar Compra")
-        # Campo para consultar informações de compras
-        produto_consulta = st.text_input("Produto para consulta")
-        if st.button("Consultar"):
-            compra_data = read_data("compras", query=produto_consulta)  # Função para ler dados com consulta
-            st.write(compra_data)
-
-    # Aba 4 - Alterar Informação
-    elif st.sidebar.radio("Selecione uma aba", ["Relatório de Compras", "Inserir Informações", "Consultar Compra", "Alterar Informação"]) == "Alterar Informação":
-        st.subheader("Alterar Informação de Compras")
-        # Campos para alterar informações de compras
-        produto_alterar = st.text_input("Produto a ser alterado")
-        nova_quantidade = st.number_input("Nova Quantidade", min_value=1)
-        novo_preco = st.number_input("Novo Preço", min_value=0.0, format="%.2f")
-
-        if st.button("Alterar"):
-            update_data("compras", produto_alterar, {"quantidade": nova_quantidade, "preco": novo_preco})  # Função para atualizar dados
-            st.success("Informações alteradas com sucesso!")
+    if st.button("Salvar"):
+        # Logic to save agency information to Google Sheets
+        if numero_milheiro_compra and quantidade_milhas_compra and programa_compra and preco_1k_milhas_compra and qtd_emissoes_compra and saldo_utilizado_compra and saldo_remanescente_compra and valor_total_compra  and taxa_compra and valor_total_pix_compra and pix_enviado_compra:
+            compra_code = len(gs.read_sheet('Controle de Compras')['Numero Compra'].values) + 1
+            compra_code = f"C{compra_code}"
+            values=[[compra_code,numero_milheiro_compra, quantidade_milhas_compra, programa_compra, preco_1k_milhas_compra, qtd_emissoes_compra, saldo_utilizado_compra, saldo_remanescente_compra, valor_total_compra, taxa_compra, valor_total_pix_compra, pix_enviado_compra]]
+            gs.insert_data_row_sheet('Controle de Compras',values)
+            st.success("Compra inserida com sucesso!")
+        else:
+            st.error("Por favor, preencha todos os campos.")
